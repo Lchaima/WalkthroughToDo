f6e76b2ad52a9c43c659e8ff46a4a4c2b87c439c
Lchaima <chaima.lihiouel@gmail.com>
2023-03-13 10:55:49 +0200

    Initial files or the project

---
+++ app/src/main/java/com/example/walkthroughtodo/viewmodel/TodoViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.walkthroughtodo.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.walkthroughtodo.model.Todo
+import com.example.walkthroughtodo.model.TodosApi
+import kotlinx.coroutines.launch
+
+
+class TodoViewModel : ViewModel() {
+    var todos = mutableStateListOf <Todo>()
+        private set
+
+    init {
+        getTodosList()
+    }
+
+    private fun getTodosList(){
+        viewModelScope.launch{
+            var todosApi: TodosApi? = null
+            try {
+                todosApi = TodosApi!!.getInstance()
+                todos.clear()
+                todos.addAll(todosApi.getTodos())
+            }catch (e:Exception) {
+                Log.d("TODOVIEWMODEL", e.message.toString())
+            }
+        }
+    }
+}
